e29247771bbd6ef36997b53a1929b57e37616e58
saranyellanki <saran.yellanki12@gmail.com>
2021-11-16 12:13:21 +0530

    Refactor initial setup project and added addHotel method to add hotels

---
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -0,0 +1,49 @@
+package com.bridgelabz;
+
+public class Hotel {
+    protected String hotelName;
+    protected int weekdayRewardRate;
+    protected int weekendRewardRate;
+    protected int weekdayRegularRate;
+    protected int weekendRegularRate;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getWeekdayRewardRate() {
+        return weekdayRewardRate;
+    }
+
+    public void setWeekdayRewardRate(int weekdayRewardRate) {
+        this.weekdayRewardRate = weekdayRewardRate;
+    }
+
+    public int getWeekendRewardRate() {
+        return weekendRewardRate;
+    }
+
+    public void setWeekendRewardRate(int weekendRewardRate) {
+        this.weekendRewardRate = weekendRewardRate;
+    }
+
+    public int getWeekdayRegularRate() {
+        return weekdayRegularRate;
+    }
+
+    public void setWeekdayRegularRate(int weekdayRegularRate) {
+        this.weekdayRegularRate = weekdayRegularRate;
+    }
+
+    public int getWeekendRegularRate() {
+        return weekendRegularRate;
+    }
+
+    public void setWeekendRegularRate(int weekendRegularRate) {
+        this.weekendRegularRate = weekendRegularRate;
+    }
+}
--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,7 +1,42 @@
 package com.bridgelabz;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class HotelReservationSystem {
+    HashMap<String ,Hotel> hotels = new HashMap<>();
+
+    /**
+     * This method is used to add hotels to hotel reservation system
+     * Can add any number of hotels till he chooses Exit option
+     */
+    public void addHotel() {
+        Scanner sc = new Scanner(System.in);
+        boolean isExit = false;
+        while (!isExit){
+            System.out.println("Enter \n1.Add Hotel \n2.Exit");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice){
+                case 1 -> {
+                    Hotel hotel = new Hotel();
+                    System.out.println("Enter hotel name");
+                    String hotelName = sc.nextLine();
+                    hotel.setHotelName(hotelName);
+                    hotels.put(hotelName, hotel);
+                }
+                default -> {
+                    isExit = true;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("====================================");
         System.out.println("Welcome to Hotel Reservation System");
+        System.out.println("====================================");
+        HotelReservationSystem hotelObj = new HotelReservationSystem();
+        hotelObj.addHotel();
     }
 }
