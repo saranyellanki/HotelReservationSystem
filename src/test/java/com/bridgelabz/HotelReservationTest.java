2016aa473811817d71aef4983870eb48d7db1ec7
saranyellanki <saran.yellanki12@gmail.com>
2021-11-17 23:11:19 +0530

    Refactor best rated hotel to add special rates to reward customers

---
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HotelReservationTest {
+    HotelReservationSystem hotel;
+    @Before
+    public void initializeClass(){
+        hotel = new HotelReservationSystem();
+    }
+    @Test
+    public void givenHotelsWhenAddedShouldReturnBestRatedHotelPrice(){
+        hotel.addHotel();
+        Assert.assertEquals(370,hotel.bestRatedHotel("2020-09-11","2020-09-12"));
+    }
+}
